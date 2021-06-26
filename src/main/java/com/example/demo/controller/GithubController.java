5dd8842c2b7623b6ee3dc052d924239e0910415c
tooPieElf <ndimnezechukwudi@gmail.com>
2021-06-26 11:26:54 +0100

    basic clean up

--- src/main/java/com/example/demo/controller/GithubController.java
+++ src/main/java/com/example/demo/controller/GithubController.java
@@ -1,47 +1,51 @@
 package com.example.demo.controller;
-
-import com.example.demo.models.Commiters;
 import com.example.demo.models.GitHubUsers;
 import com.example.demo.services.GithubService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-@RestController()
+@RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class GithubController {
 
-    @Autowired
-    GithubService githubService;
+   private final GithubService githubService;
 
 
     @GetMapping("/getRepositories/{repoName}")
-    public ResponseEntity<GitHubUsers> getRepositories(@PathVariable String repoName){
-       return ResponseEntity.ok(githubService.getRepositories(repoName));
+    public GitHubUsers getRepositories(@PathVariable String repoName){
+       return githubService.getRepositories(repoName);
     }
 
     @GetMapping("/getUniqueCommiter/{base}/{repoName}")
-    public ResponseEntity<Set<String>> getUniqueCommiters(@PathVariable String base, @PathVariable String repoName){
-        return ResponseEntity.ok(githubService.getUniqueCommiters(base,repoName));
+    public Set<String> getUniqueCommiters(@PathVariable String base, @PathVariable String repoName){
+        return githubService.getUniqueCommiters(base,repoName);
     }
 
     @GetMapping("/getCommiterImpact/{base}/{repoName}")
-    public ResponseEntity<Map<String,Integer>> getCommiterImpact(@PathVariable String base, @PathVariable String repoName){
-        return ResponseEntity.ok(githubService.getCommitterImpact(base,repoName));
+    public Map<String,Integer> getCommiterImpact(@PathVariable String base, @PathVariable String repoName){
+        return githubService.getCommitterImpact(base,repoName);
     }
 
 
     @GetMapping("/getCommitTimeline/{base}/{repoName}")
-    public ResponseEntity<Map<String,Integer>> getCommitTimeline(@PathVariable String base, @PathVariable String repoName){
-        return ResponseEntity.ok(githubService.getCommitTimeline(base,repoName));
+    public Map<String,Integer> getCommitTimeline(@PathVariable String base, @PathVariable String repoName){
+        return githubService.getCommitTimeline(base,repoName);
     }
 
+    @ExceptionHandler(RuntimeException.class)
+    public final ResponseEntity<Exception> handleAllExceptions(RuntimeException ex) {
+        return new ResponseEntity<Exception>(ex, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
--- src/main/java/com/example/demo/services/GithubService.java
+++ src/main/java/com/example/demo/services/GithubService.java
@@ -1,71 +1,54 @@
 package com.example.demo.services;
 
-import com.example.demo.models.Author;
-import com.example.demo.models.Commit;
-import com.example.demo.models.Commiters;
 import com.example.demo.models.GitHubUsers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 
 @Service
+@RequiredArgsConstructor
 public class GithubService {
 
-    @Autowired
-    RestTemplate restTemplate;
+
+   private final RestTemplate restTemplate;
+
+   private String userUrl = "https://api.github.com/search/repositories?q=";
 
     public GitHubUsers getRepositories(String repoName){
-        return restTemplate.getForObject("https://api.github.com/search/repositories?q="+repoName, GitHubUsers.class);
+        return restTemplate.getForObject(userUrl+repoName, GitHubUsers.class);
     }
 
     public Set<String> getUniqueCommiters(String base, String repoName){
         List<String> names = getCommitNames(base, repoName);
-        Set<String> uniqueNames = new HashSet<>(names);
-        return uniqueNames;
+        return new HashSet<>(names);
     }
 
     public Map<String,Integer> getCommitterImpact(String base, String repoName){
         Map<String, Integer> impacts = new HashMap<>();
         List<String> names = getCommitNames(base, repoName);
-
         PutInHashMap(impacts, names);
-
         return impacts;
     }
 
     public Map<String, Integer> getCommitTimeline(String base, String repoName){
         Map<String, Integer> commitTimeline = new HashMap<>();
-
-        List<LinkedHashMap<String, LinkedHashMap<String, LinkedHashMap<String, String>>>> commitersList =
-                restTemplate.getForObject("https://api.github.com/repos/"+base+"/"+repoName+"/commits?per_page=100", List.class);
+        List<LinkedHashMap<String, LinkedHashMap<String, LinkedHashMap<String, String>>>> commitersList = getLinkedHashMaps(base, repoName);
         List<String> commitDates = commitersList.stream()
                 .map(object->object.get("commit"))
                 .map(commit->commit.get("author"))
                 .map(authors->authors.get("date").substring(0,10))
                 .collect(Collectors.toList());
-
         PutInHashMap(commitTimeline, commitDates);
-
         return commitTimeline;
-
-
     }
 
     public void PutInHashMap(Map<String, Integer> commitTimeline, List<String> commitDates) {
@@ -73,7 +56,7 @@ public class GithubService {
             if(commitTimeline.containsKey(s)){
                 Integer value =commitTimeline.get(s);
                 value +=1;
-                commitTimeline.put(s,value+1);
+                commitTimeline.put(s,value);
             }else{
                 commitTimeline.put(s, 1);
             }
@@ -82,13 +65,17 @@ public class GithubService {
 
 
     public List<String> getCommitNames(String base, String repoName){
-        List<LinkedHashMap<String, LinkedHashMap<String, LinkedHashMap<String, String>>>> commitersList =
-                restTemplate.getForObject("https://api.github.com/repos/"+base+"/"+repoName+"/commits?per_page=100", List.class);
+        List<LinkedHashMap<String, LinkedHashMap<String, LinkedHashMap<String, String>>>> commitersList = getLinkedHashMaps(base, repoName);
         return commitersList.stream()
                 .map(object->object.get("commit"))
                 .map(commit->commit.get("author"))
                 .map(authors->authors.get("name"))
                 .collect(Collectors.toList());
+    }
+
+    public List<LinkedHashMap<String, LinkedHashMap<String, LinkedHashMap<String, String>>>> getLinkedHashMaps(String base, String repoName) {
+        String commitUrl = "https://api.github.com/repos/"+ base + "/" + repoName + "/commits?per_page=100";
+        return restTemplate.getForObject( commitUrl, List.class);
     }
 
 }
